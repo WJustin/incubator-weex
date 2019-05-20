8edaa27b31404c3bf3ca6a1f863df917bb759ecd
Justin.wang <wangrongchao@126.com>
2019-05-20 15:29:54 +0800

    add demo

---
+++ ios/sdk/Example/WeexSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeexSDKDemo
+//
+//  Created by Justin.wang on 2019/5/20.
+//  Copyright © 2019年 只二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
